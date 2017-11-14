83a1944e19d949467795dd3970f115b7f663648f
Kevin Glueck <kglueck@viz.tamu.edu>
2017-11-14 11:05:27 -0600

    collapse config_edit.rb into default recipe

--- recipes/default.rb
+++ recipes/default.rb
@@ -29,4 +29,18 @@ ark 'duoproxy' do
   action :install
 end
 
-include_recipe 'duoproxy::config_edit'
+template '/opt/duoproxy-1/conf/authproxy.cfg' do
+  source 'default.erb'
+  owner 'root'
+  group 'root'
+  variables(
+    host: node['duoproxy']['host'],
+    host_2: node['duoproxy']['host_2'],
+    service_account_username: node['duoproxy']['service_account_username'],
+    service_account_password: node['duoproxy']['service_account_password'],
+    search_dn: node['duoproxy']['search_dn'],
+    ikey: node['duoproxy']['ikey'],
+    skey: node['duoproxy']['skey'],
+    api_host: node['duoproxy']['api_host']
+  )
+end
