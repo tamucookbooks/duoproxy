64167e22813769240f08f732e82d0007297d05b2
lauramelton672 <lauramelton672@tamu.edu>
2017-11-13 21:14:50 -0600

    correcting issues with attributes

---
+++ attributes/default..rb
@@ -0,0 +1,8 @@
+default['duoproxy']['host'] = '127.0.0.1'
+default['duoproxy']['host_2'] = '127.0.0.2'
+default['duoproxy']['service_account_username'] = 'duo'
+default['duoproxy']['service_account_password'] = '123456789'
+default['duoproxy']['search_dn'] = 'CN=USERS,DC=CONTOSO,DC=COM'
+default['duoproxy']['ikey'] = 'PRIVATEKEY123'
+default['duoproxy']['skey'] = 'PRIVATEPASS1234!'
+default['duoproxy']['api_host'] = 'api-56758764654.duosecurity.com'
--- recipes/config_edit.rb
+++ recipes/config_edit.rb
@@ -1,15 +1,15 @@
 template '/opt/duoproxy-1/conf/authproxy.cfg' do
-  source 'authproxy.erb'
+  source 'default.erb'
   owner 'root'
   group 'root'
   variables(
-    host:  '127.0.0.1',
-    host_2: '127.0.0.2',
-    service_account_username: 'duo',
-    service_account_password: '123456789',
-    search_dn: 'CN=USERS,DC=CONTOSO,DC=COM ',
-    ikey: 'PRIVATEKEY123',
-    skey: 'PRIVATEPASS1234!',
-    api_host: 'api-56758764654.duosecurity.com'
+    host: node['duoproxy']['host'],
+    host_2: node['duoproxy']['host_2'],
+    service_account_username: node['duoproxy']['service_account_username'],
+    service_account_password: node['duoproxy']['service_account_password'],
+    search_dn: node['duoproxy']['search_dn'],
+    ikey: node['duoproxy']['ikey'],
+    skey: node['duoproxy']['skey'],
+    api_host: node['duoproxy']['api_host']
   )
 end
